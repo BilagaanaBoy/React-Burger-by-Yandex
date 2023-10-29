b65748ec92bd9e191e641f8f8f17d6d78baf7dc5
BilagaanaBoy <losharals@yandex.ru>
2023-10-29 17:20:42 +0300

    sprint-1-step-2

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+ import App from './components/App/App';
+// import reportWebVitals from './components/App/reportWebVitals';
+//import reportWebVitals from '/React/react-burger/src/components/App/reportWebVitals';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
