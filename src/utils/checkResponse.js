7b704dfa21607fe0ef4bd3ea0822cce4ca35b33c
BilagaanaBoy <148550442+BilagaanaBoy@users.noreply.github.com>
2023-11-14 12:31:41 +0300

    Delete src directory

--- src/utils/checkResponse.js
+++
@@ -1,8 +0,0 @@
-function checkResponse(response) {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(`Ошибка ${response.status}`);
-  }
-
-export default checkResponse;
