8c48978fd95345b5b0dab43d2afd98ad6dd793cf
BilagaanaBoy <148550442+BilagaanaBoy@users.noreply.github.com>
2023-12-11 13:31:10 +0300

    sprint-4-step-2

---
+++ src/components/IngredientConstructor/IngredientConstructor.tsx
@@ -0,0 +1,74 @@
+import { FunctionComponent, useRef } from 'react';
+import { useDrop, useDrag } from 'react-dnd';
+import { useDispatch } from 'react-redux';
+
+import styles from './IngredientConstructor.module.css';
+
+import PropTypes from 'prop-types';
+import { TItem } from '../../utils/types';
+
+import { DELETE_INGREDIENT } from '../../services/actions';
+import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+interface IIngredientConstructor  {
+  item: TItem;
+  index: number;
+  dragElement: (dragIndex: number, hoverIndex: number) => void;
+}
+
+const  IngredientConstructor: FunctionComponent<IIngredientConstructor> = (props) => {
+  const { item, index, dragElement } = props;
+  const dispatch = useDispatch();
+  const ref = useRef<HTMLInputElement>(null);
+  
+
+  const [ , dragRef] = useDrag({
+    type: 'constructorElement',
+    item: () => {
+      return { item, index };
+    }
+  });
+
+  const [, dropRef] = useDrop({
+    accept: 'constructorElement',
+    hover: (item: TItem, monitor) => {
+      if (!item.index || item.index === index) 
+        return;
+      if (!ref.current) return;
+      let hoverMiddle = (ref.current.getBoundingClientRect().bottom - ref.current.getBoundingClientRect().top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      if (!clientOffset) return;
+      let hoverUser = clientOffset.y - ref.current.getBoundingClientRect().top;
+
+      if (item.index < index && hoverUser < hoverMiddle) 
+        return;
+      if (item.index > index && hoverUser > hoverMiddle) 
+        return;
+      dragElement(item.index, index);
+      item.index = index;
+    },
+  });
+  dragRef(dropRef(ref));
+
+  function deleteIngredient(item: TItem) {
+    dispatch({
+      type: DELETE_INGREDIENT,
+      item: item,
+      qnt: 1,
+    });
+  }
+
+  return (
+    <div ref={ref} className={styles.flex} key={item.uniqueId} >
+      <DragIcon type="primary" />
+      <ConstructorElement
+        handleClose={() => deleteIngredient(item)}
+        text={item.name}
+        price={item.price}
+        thumbnail={item.image_mobile}
+      />
+    </div>
+  );
+}
+
+export default IngredientConstructor;
---
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useHistory, Redirect } from 'react-router-dom';
+
+import { EmailInput,Button} from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './ForgotPassword.module.css';
+import clsx from 'clsx';
+
+import { forgotPassword } from '../../utils/auth';
+
+function ForgotPassword() {
+  const [form, setValue] = useState({ email: '' });
+  const history = useHistory();
+  const auth = useSelector((store: any) => store.authReducer.isAuthorized);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const passwordReset = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      forgotPassword(form.email)
+        .then((data) => {
+          if (data.success) {
+            history.replace({ pathname: '/reset-password' });
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    },
+    [form, history]
+  );
+
+  if (auth) {
+    return <Redirect to={'/'} />;
+  }
+
+  return (
+    <div className={styles.main}>
+      <form
+        className={clsx(styles.form,'mb-20')}
+        onSubmit={passwordReset}
+      >
+        <h1 className={'text text_type_main-large mb-6'}>
+          Восстановление пароля
+        </h1>
+        <div className={'mb-6'}>
+          <EmailInput
+            value={form.email}
+            name="email"
+            onChange={onChange}
+          />
+        </div>
+        {/* @ts-ignore */}
+        <Button type='primary'>Восстановить</Button>
+      </form>
+      <div className={styles.flex}>
+        <p className={'text text_type_main-default text_color_inactive'}>
+          Вспомнили пароль?
+        </p>
+        <Link
+          to="/login"
+          className={clsx(styles.link,' text text_type_main-default ml-2')}
+        >
+          Войти
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPassword;
