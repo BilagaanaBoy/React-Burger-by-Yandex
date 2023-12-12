94563db08d4c4ef3eed1dd1ae06b0b87f26fb63a
BilagaanaBoy <148550442+BilagaanaBoy@users.noreply.github.com>
2023-12-12 12:56:36 +0300

    Fix

---
+++ src/components/NotFound404/NotFound404.tsx
@@ -0,0 +1,10 @@
+function NotFound404() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <p className="text text_type_digits-large mt-20">ERROR 404 !</p>
+      <p className="text text_type_main-large mt-10">Страницы не существует</p>
+    </div>
+  );
+}
+
+export default NotFound404;
